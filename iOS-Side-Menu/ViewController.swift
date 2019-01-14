deef3457b6dec6b281dcebf57852f607247c1d70
Vishwanath Deshmukh <vishwanath.deshmukh@simplisafe.com>
2019-01-14 10:54:18 -0500

    Created Single view app

---
+++ iOS-Side-Menu/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS-Side-Menu
+//
+//  Created by Vishwanath Deshmukh on 14/01/2019.
+//  Copyright © 2019 Vishwanath Deshmukh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
